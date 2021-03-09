8939d42f841e928929f8aaf276af4bc7d2dd6b43
Richard Basdeo <basdeorichard@gmail.com>
2021-03-09 13:54:59 -0500

    Can view post in feed - Richard

---
+++ instagramClone/View/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  instagramClone
+//
+//  Created by Richard Basdeo on 3/9/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var captionNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
